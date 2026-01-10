0058ab9a69bb5348d84c5dce99eeb6b87c281f6d
bbmilana <bbmilana66@gmail.com>
2026-01-11 01:12:15 +0600

    Fix clock.py for Telegram avatar 4

--- clock.py
+++ clock.py
@@ -4,35 +4,33 @@ from telethon import TelegramClient, functions
 from telethon.sessions import StringSession
 import os
 
+# Берём значения из секретов
 api_id = int(os.environ["API_ID"])
 api_hash = os.environ["API_HASH"]
 session = os.environ["SESSION_STRING"]
 
-# создаём квадратное изображение 512x512
+# создаём квадратное изображение 512x512 RGB
 img = Image.new("RGB", (512, 512), "black")
 draw = ImageDraw.Draw(img)
 
+# текущее время
 now = datetime.now().strftime("%H:%M")
 
-# используем шрифт Ubuntu
-try:
-    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 120)
-except:
-    font = ImageFont.load_default()
+# используем шрифт Ubuntu (есть на GitHub Actions)
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 150)
 
-# вычисляем ширину и высоту текста через textbbox
+# вычисляем размеры текста по центру
 bbox = draw.textbbox((0, 0), now, font=font)
 w = bbox[2] - bbox[0]
 h = bbox[3] - bbox[1]
 
-# рисуем текст по центру
-draw.text(((512-w)/2, (512-h)/2), now, fill="white", font=font)
+draw.text(((512 - w)/2, (512 - h)/2), now, fill="white", font=font)
 
-# сохраняем картинку
-img.save("avatar.jpg")
+# сохраняем PNG
+img.save("avatar.png")
 
-# подключаемся к аккаунту и обновляем аватар
+# подключаемся к аккаунту и меняем аватарку
 with TelegramClient(StringSession(session), api_id, api_hash) as client:
     client.start()
-    file = client.upload_file("avatar.jpg")
+    file = client.upload_file("avatar.png")
     client(functions.photos.UploadProfilePhotoRequest(file=file))
